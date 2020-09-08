a3f0d8095559a556150896ea05c889317b50bc61
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-07 21:13:58 -0400

    I can view all roles, departments and employees.

---
+++ portal.js
@@ -0,0 +1,106 @@
+// DEPENDENCIES
+// ========================
+var mySql = require("mysql");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table")
+
+// SETTING UP MYSQL DATABASE CONNECTION
+// ===================================================
+var connection = mySql.createConnection({
+    host: "localhost",
+
+    // Port
+    port: 3306,
+
+    // Username
+    user: "root",
+
+    // Password
+    password: "Aqua5961!",
+    database: "employee_portal"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    portalPrompt();
+});
+
+// CREATING ALL PROMPTS FOR THE CLI THAT THE USER WILL EXPERIENCE
+// =============================================================================
+const portalPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Welcome to the employee portal! What would you like to do?",
+            name: "Portal",
+            choices: [
+                "View All Employees",
+                "Add A New Employee",
+                "View All Departments",
+                "Add A New Department",
+                "View All Roles",
+                "Add A New Role",
+                "Promote/Demote Employee"
+            ],
+        }
+    ]).then(answers => {
+        console.table(answers);
+
+        // GENERATING WHAT POPULATES ON THE CLI BY CALLING THE FUNCTION BELOW
+        // =================================================================================
+        switch (answers.Portal) {
+            case "View All Employees":
+                employees();
+                connection.end(); // ALLOWS ME TO NOT HAVE TO CTRL+C EVERYTIME TO BE ABLE TO DO SOMETHING ELSE WITH THE TABLES
+                break;
+         
+            case "View All Departments":
+                departments();
+                connection.end(); // ALLOWS ME TO NOT HAVE TO CTRL+C EVERYTIME TO BE ABLE TO DO SOMETHING ELSE WITH THE TABLES
+                break;
+
+            case "View All Roles":
+                roles();
+                connection.end(); // ALLOWS ME TO NOT HAVE TO CTRL+C EVERYTIME TO BE ABLE TO DO SOMETHING ELSE WITH THE TABLES
+                break;
+        }
+
+    })
+}
+
+// CREATING FUNCTIONS POPULATE WHAT IS SELECTED FROM THE DATABASE
+// =================================================================================
+function employees() {
+    // NPM MYSQL DOCUMENTATION UNDER "POOLING CONNECTIONS" SHOWED ME THE FOLLOWING LINE
+    connection.query("SELECT * FROM employees", (err, res) => {
+        // =========================================================
+        if (err) {
+            throw err;
+        }
+        console.table(res);
+    });
+}
+
+function departments() {
+    // NPM MYSQL DOCUMENTATION UNDER "POOLING CONNECTIONS" SHOWED ME THE FOLLOWING LINE
+    connection.query("SELECT * FROM departments", (err, res) => {
+        // =========================================================
+        if (err) {
+            throw err;
+        }
+        console.table(res);
+    });
+}
+
+
+function roles() {
+    // NPM MYSQL DOCUMENTATION UNDER "POOLING CONNECTIONS" SHOWED ME THE FOLLOWING LINE
+    connection.query("SELECT * FROM company_role", (err, res) => {
+        // =========================================================
+        if (err) {
+            throw err;
+        }
+        console.table(res);
+    });
+}
---
+++ seed.sql
@@ -0,0 +1,48 @@
+-- CREATING DATABASE --
+DROP DATABASE IF EXISTS employee_portal;
+CREATE DATABASE employee_portal;
+USE employee_portal;
+
+
+-- CREATING TABLES BELOW --
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    role_name VARCHAR(30),
+    department_id INT,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE company_role (
+    role_id INT PRIMARY KEY NOT NULL,
+    role_name VARCHAR(30)
+);
+
+-- CREATING VALUES TO BE INSERTED INTO TABLES ABOVE --
+INSERT INTO employees (employee_id, first_name, last_name, department_id, department_name, role_name, role_id)
+VALUES (1, "Adrian", "Diaz", 1, "Board Of Directors", "CEO", 1), 
+(2, "Emily", "Tamayo", 1, "Board Of Directors", "CFO", 2), 
+(3, "Wanda", "Diaz", 1, "Board Of Directors", "COO", 3), 
+(4, "Daniel", "Diaz", 1, "Board Of Directors", "CIO", 4), 
+(5, "Deni", "Diaz", 1, "Board Of Directors", "CMO", 5);
+
+INSERT INTO departments (department_id, department_name)
+VALUES (1, "Board of Directors");
+
+INSERT INTO company_role (role_name, role_id)
+VALUES ("CEO", 1),
+("CFO", 2),
+("COO", 3),
+("CIO", 4),
+("CMO", 5);
+
+SELECT*FROM employees;
+SELECT*FROM departments;
+SELECT*FROM company_role;
